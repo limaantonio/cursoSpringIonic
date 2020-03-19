7f947e1b5b880795020d247597cba1945cf51a1c
limaantonio <limaantoniocarlos20@gmail.com>
2020-03-19 01:32:08 -0300

    Primeiro commit, crição do projeto.

---
+++ src/main/java/com/ac/curso/CursoSpringIonicApplication.java
@@ -0,0 +1,15 @@
+package com.ac.curso;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ac/curso/CursoSpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
